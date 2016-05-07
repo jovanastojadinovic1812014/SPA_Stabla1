b3076e476d74c49dd915f6e7a57dd1c5d55052bc
jovanastojadinovic1812014 <jovanastojadinovic@hotmail.com>
2016-05-07 11:51:22 +0200

    BST stablo zadatak

---
+++ Stojadinovic_Jovana_2014-0181_3/src/main/BSTStablo.java
@@ -0,0 +1,18 @@
+package main;
+
+import labis.cvorovi.CvorStabla;
+import labis.exception.LabisException;
+import labis.stabla.ABSTStablo;
+
+public class BSTStablo extends ABSTStablo {
+
+	@Override
+	public void ispisiRastuce(CvorStabla k) throws LabisException {
+		if (k == null) {
+			return;
+		}
+		ispisiRastuce(k.levo);
+		System.out.println(k.podatak + " ");
+		ispisiRastuce(k.desno);
+	}
+}
